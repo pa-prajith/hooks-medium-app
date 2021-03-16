03bb6f27b7854b2bb84603fb62a32bbe11d53c93
pa-prajith <codejs100@gmail.com>
2021-03-16 16:19:58 -0700

    Added UserContext

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
 
 const App = () => {
   return (
     <div className="container">
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
+      <CurrentUserProvider>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserProvider>
     </div>
   )
 }
